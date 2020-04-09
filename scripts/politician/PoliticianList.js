29e4a1c933207b3d9bfcc1ae58e25d5417a2bca6
Keartroth <mecarrolljr@gmail.com>
2020-04-09 17:21:53 -0500

    edited PoliticianList.js to filter related resources on database.json to find all PAC donations relevant to each politician; edited Politician.js to list all PAC donations.

--- scripts/politician/PoliticianList.js
+++ scripts/politician/PoliticianList.js
@@ -1,5 +1,7 @@
 import { usePoliticians } from "./politicianProvider.js";
 import { Politician } from "./Politician.js";
+import { usePACs } from "../pacs/pacProvider.js";
+import { usePACDonations } from "./pacDonationsProvider.js";
 
 const contentTarget = document.querySelector("#politicians");
 
@@ -9,6 +11,19 @@ const render = (politiciansArray) => {
 
 export const PoliticianList = () => {
     const allThePolititians = usePoliticians();
+    const allThePACs = usePACs();
+    const allThePACDonations = usePACDonations();
+
     contentTarget.innerHTML = "<h3>List of Politicians</h3>"
+    allThePolititians.map(p => {
+        p.donors = [];
+        allThePACDonations.filter(d => d.politicianId === p.id).map(object => {
+            let donorObject ={};
+            let foundPAC = allThePACs.find(cp => cp.id === object.pacId);
+            donorObject.name = foundPAC.registeredName;
+            donorObject.amount = object.amount;
+            p.donors.push(donorObject);
+        })
+    })
     render(allThePolititians);
 }
